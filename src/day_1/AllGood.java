064d70aad1fb8b3072a5670c4f861e2b872d1fed
Pettar <64172282+pattar113@users.noreply.github.com>
2020-04-24 13:29:56 -0700

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,6 +3,9 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
         //I HOPE EVERYTHING IS GOOD.
+        
+        
+        System.out.println("Welcome to GitHub");
 
 
 
